b6f74147bfd239bbb4e2d4c9199a3eb2e9ac2163
Gonzalo Nandez <godan2310@yahoo.es>
2017-06-20 03:51:18 +0200

    Post 16

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,6 +15,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-//Vue.component('example', require('./components/Example.vue'));
+Vue.component('fecha', require('./components/datepicker.vue'));
 
 
